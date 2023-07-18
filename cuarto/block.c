29307f504dab1f54e7afef34d40b3fb9045c5618
jafeth <jafethrafael12@gmail.com>
2023-07-18 17:00:00 +0200

    resulta que los vertices estaban mal xddd

--- cuarto/block.c
+++ cuarto/block.c
@@ -19,16 +19,16 @@ unsigned int BLOCK_VERTICES_SIZE = 24 * sizeof(float);
 unsigned int BLOCK_INDICES_SIZE = 36 * sizeof(unsigned int);
 
 float BLOCK_VERTICES[] = {
-    1, 0, 1,        // 0 -> North east at Y=0 (arriba derecha)
-    1, 0, -1,       // 1 -> South east at Y=0 (abajo derecha)
-    -1, 0, -1,      // 2 -> South west at Y=0 (abajo izquierda)
-    -1, 0, 1,       // 3 -> North west at Y=0 (arriba izquierda)
+    1, 0, 0,        // 0 -> North east at Y=0 (arriba derecha)
+    1, 0, 1,        // 1 -> South east at Y=0 (abajo derecha)
+    0, 0, 1,        // 2 -> South west at Y=0 (abajo izquierda)
+    0, 0, 0,        // 3 -> North west at Y=0 (arriba izquierda)
 
     // same but Y=1
-    1, 1, 1,        // 4
-    1, 1, -1,       // 5
-    -1, 1, -1,      // 6
-    -1, 1, 1,       // 7
+    1, 1, 0,        // 4
+    1, 1, 1,        // 5
+    0, 1, 1,        // 6
+    0, 1, 0,        // 7
 };
 
 unsigned int BLOCK_INDICES[] = {
